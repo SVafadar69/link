db1dd8276a05cfb1bda3fe391a9d35f311a1375d
SVafadar69 <svafadar19@gmail.com>
2026-05-21 23:04:06 -0600

    single camera - flipped image

--- backend/main.cpp
+++ backend/main.cpp
@@ -1,77 +1,32 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
 #include <iostream>
-#include <string>
-
 #include "BrightnessEstimator.hpp"
 
 int main() {
-    BrightnessThresholds thresholds;
-    thresholds.Night = 0;
-
-    const int MAX_STREAMS = 6;
-
-    std::vector<cv::VideoCapture> caps;
-    std::vector<int> camIds;
-
-    for (int i = 0; i < 4 && static_cast<int>(caps.size()) < MAX_STREAMS; ++i) {
-        cv::VideoCapture cap(i, cv::CAP_ANY);
-
-        if (cap.isOpened() && cap.grab()) {
-            std::cout << "Camera index " << i << " opened successfully\n";
-
-            caps.push_back(std::move(cap));
-            camIds.push_back(i);
-        }
-        else {
-            std::cout << "Camera index " << i << " not available\n";
-        }
+    const int camera_index = 0;
+    cv::VideoCapture cap(camera_index, cv::CAP_ANY);
+    if (!cap.isOpened()) {
+        std::cerr << "Error: Could not open camera " << camera_index << "\n";
+        return -1;
     }
-
-    while (static_cast<int>(caps.size()) < MAX_STREAMS) {
-        caps.emplace_back();
-        camIds.push_back(-1);
-    }
-
-    cv::Mat placeholder(
-        480,
-        640,
-        CV_8UC3,
-        cv::Scalar(0, 0, 255)
-    );
+    cv::Mat frame; 
+    cv:: Mat flippedFrame; 
 
     while (true) {
-        for (int i = 0; i < MAX_STREAMS; ++i) {
-            std::string title = "Slot " + std::to_string(i);
-
-            cv::Mat frame;
-
-            if (camIds[i] != -1 && caps[i].isOpened()) {
-                caps[i].read(frame);
-				if (camIds[i] == 2) {
-					cv::imshow(title, frame);
-				}
-				double frame_brightness = calculateBrightness(frame);
-				std::cout << "Frame Brightness" << std::to_string(frame_brightness) << std::endl;
-				//bool dark = isDark(frame_brightness, thresholds);
-				//std::cout << "Is Dark: " << std::to_string(dark) << std::endl;
-			}
-
-            
+        cap >> frame; 
+        if (frame.empty()) {
+            std::cerr << "Error: empty frame"; 
+            break;
         }
-
-        if (cv::waitKey(1) == 'q') {
+        cv::rotate(frame, flippedFrame, cv::ROTATE_90_COUNTERCLOCKWISE);
+        cv::imshow("Camera Feed", flippedFrame);
+        char key = static_cast<char>(cv::waitKey(1));
+        if (key == 'q') {
             break;
         }
     }
-
-    for (auto& cap : caps) {
-        if (cap.isOpened()) {
-            cap.release();
-        }
-    }
-
+    cap.release();
     cv::destroyAllWindows();
-
     return 0;
 }
